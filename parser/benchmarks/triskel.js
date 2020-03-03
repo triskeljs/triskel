69ff11e80c23718e54f69b815f57a160e01395ea
Jesús Germade <jesus@germade.es>
2020-03-03 19:51:55 +0100

    lint tweaks

--- parser/benchmarks/triskel.js
+++ parser/benchmarks/triskel.js
@@ -18,9 +18,11 @@ var bench = benchmark(function (html, callback) {
 })
 
 bench.on('progress', function (key) {
+  // eslint-disable-next-line no-console
 	console.log('finished parsing ' + key + '.html')
 })
 
 bench.on('result', function (stat) {
+  // eslint-disable-next-line no-console
 	console.log(stat.mean().toPrecision(6) + ' ms/file ± ' + stat.sd().toPrecision(6))
 })
